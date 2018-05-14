defd854f40e8df10e0672bd5b42446042eab4bc5
unknown <melissa@txable.com>
2018-05-13 22:34:53 -0400

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+INSERT INTO burgers (burger_name) VALUES ('The Western');
+INSERT INTO burgers (burger_name) VALUES ('The Classic');
+INSERT INTO burgers (burger_name) VALUES ('The Spicy Goat');
+INSERT INTO burgers (burger_name) VALUES ('The Kevin Bacon');
+
+INSERT INTO menu (item) VALUES ('The Western');
+INSERT INTO menu (item) VALUES ('The Classic');
+INSERT INTO menu (item) VALUES ('The Spicy Goat');
+INSERT INTO menu (item) VALUES ('The Kevin Bacon');
+INSERT INTO menu (item) VALUES ('le Wagyu');
+INSERT INTO menu (item) VALUES ('In The Buff');
+INSERT INTO menu (item) VALUES ('Lots-a-Shroom');
+INSERT INTO menu (item) VALUES ('The Verde');
+INSERT INTO menu (item) VALUES ('Vegabond');
+INSERT INTO menu (item) VALUES ('The Happy Pilgrim');
+INSERT INTO menu (item) VALUES ('Big Tex');
+INSERT INTO menu (item) VALUES ('Freshman 15');
+INSERT INTO menu (item) VALUES ('All American');
+
+INSERT INTO menu (item, entree) VALUES ('Hand-cut Potato Chips', 0);
+INSERT INTO menu (item, entree) VALUES ('Hand-cut French Fries', 0);
+INSERT INTO menu (item, entree) VALUES ('Melted Cheese Fries', 0);
+INSERT INTO menu (item, entree) VALUES ('Fried Onion Strings, aka Crack', 0);
+INSERT INTO menu (item, entree) VALUES ("Quincy's Fried Pickles", 0);
+INSERT INTO menu (item, entree) VALUES ("Fried Green Beans n' Carrots", 0);
+INSERT INTO menu (item, entree) VALUES ("Treat yo' Self Fries", 0);
